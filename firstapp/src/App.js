59fae75f997ae3047ad3ef8ebfd43a1207d19d2e
codelaghien <codelaghien.github.io>
2021-02-27 11:04:39 -0800

    1008 - Components

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Car from './Car';
+import Xe from './Xe';
 
 function App() {
 	const html = (
@@ -17,6 +19,8 @@ function App() {
 				>
 					Learn React
 				</a>
+				<Car />
+				<Xe />
 			</header>
 		</div>
 	);
---
+++ firstapp/src/Xe.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Xe extends React.Component {
+	render() {
+		return <h2>Hi, Tôi là chiếc xe!</h2>;
+	}
+}
+
+export default Xe;
