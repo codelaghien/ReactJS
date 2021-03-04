815db12b6a844628d643f23a1e5657ccb9ae96da
codelaghien <codelaghien.github.io>
2021-03-04 11:12:50 -0800

    1012 - Events

--- firstapp/src/Xe.js
+++ firstapp/src/Xe.js
@@ -8,6 +8,8 @@ class Xe extends React.Component {
 			color: props.color,
 			value: props.value,
 		};
+		console.log('this', this);
+		// this.shoot = this.shoot.bind(this);
 	}
 
 	// static getDerivedStateFromProps(props, state) {
@@ -15,22 +17,60 @@ class Xe extends React.Component {
 	// 	return { color: props.color };
 	// }
 
+	shoot() {
+		console.log('shoot this = ', this);
+		// alert('Great Shot!');
+	}
+
+	shoot2(b) {
+		console.log('shoot 2 this = ', this);
+		console.log('shoot b = ', b);
+		// alert('Great Shot!');
+	}
+
+	shoot3 = (a) => {
+		// console.log('shoot2 this = ', this);
+		console.log('shoot 3 a = ', a);
+	};
+
 	render() {
 		console.log('render');
 		// console.log('Xe class: ', this.props.color);
 		// this.setState({ color: this.props.color, value: this.props.value });
-		return (
-			<h3 style={{ color: this.state.color }}>Hi, {this.state.value}!</h3>
+		const shoot = (
+			<h3
+				onClick={this.shoot}
+				style={{ color: this.state.color, cursor: 'pointer' }}
+			>
+				Hi, {this.state.value}!
+			</h3>
 		);
+		const shoot2 = (
+			<h3
+				onClick={this.shoot2.bind(this, 'Goal A')}
+				style={{ color: this.state.color, cursor: 'pointer' }}
+			>
+				Hi, {this.state.value}!
+			</h3>
+		);
+		const shoot3 = (
+			<h3
+				onClick={() => this.shoot3('Goal')}
+				style={{ color: this.state.color, cursor: 'pointer' }}
+			>
+				Hi, {this.state.value}!
+			</h3>
+		);
+		return shoot;
 	}
 
-	componentDidMount() {
-		console.log('componentDidMount');
-		setTimeout(() => {
-			this.setState({ color: 'yellow' });
-			console.log('componentDidMount đã thay đổi thành màu vàng');
-		}, 3000);
-	}
+	// componentDidMount() {
+	// 	console.log('componentDidMount');
+	// 	setTimeout(() => {
+	// 		this.setState({ color: 'yellow' });
+	// 		console.log('componentDidMount đã thay đổi thành màu vàng');
+	// 	}, 3000);
+	// }
 }
 
 export default Xe;
